bb3599a6341cdb53a755bafbc0d7f5ed887c6a83
zyh <2384252826@qq.com>
2022-11-28 16:36:37 +0800

    2-2

---
+++ src/main/java/com/zyh/wikikb/WikikbApplication.java
@@ -0,0 +1,13 @@
+package com.zyh.wikikb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikikbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikikbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyh/wikikb/WikikbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyh.wikikb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikikbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

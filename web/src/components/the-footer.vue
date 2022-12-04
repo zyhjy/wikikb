97edb2fc8e47d8bcc0d795dfff65b5dc13a407ae
zyh <2384252826@qq.com>
2022-12-05 01:01:16 +0800

    4-7制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        java电子书
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'the-footer',
+
+    });
+</script>
